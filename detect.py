18c9c0d552267cdeabc673ac68bc03fcdb2e56ed
charlieguo0610 <charlieguo0610@gmail.com>
2022-07-03 01:19:15 +0800

    Update detect.py

--- detect.py
+++ detect.py
@@ -66,7 +66,7 @@ def run(weights=ROOT / 'yolov3.pt',  # model.pt path(s)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
     is_file = Path(source).suffix[1:] in (IMG_FORMATS + VID_FORMATS)
     is_url = source.lower().startswith(('rtsp://', 'rtmp://', 'http://', 'https://'))
-    webcam = source.isnumeric() or source.endswith('.txt') or (is_url and not is_file)
+    webcam = source.isnumeric() or source.endswith('.txt') or (is_url and not is_file) # detect webcam
     if is_url and is_file:
         source = check_file(source)  # download
 
